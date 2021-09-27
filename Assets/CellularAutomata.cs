c1e18c5e7c31bc708a27ede71c9f89edb233ddbe
nick <nshelton@gmail.com>
2021-09-27 08:50:03 -0700

    initial commit

---
+++ Assets/CellularAutomata.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellularAutomata : MonoBehaviour
+{
+
+    ComputeShader _shader;
+
+    void Start()
+    {
+        
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+
+    }
+}
