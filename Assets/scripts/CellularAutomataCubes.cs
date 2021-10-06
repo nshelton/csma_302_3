83ab567086a698437d7bec7530f18b60cda59c71
chaneltiana <chaneltianacruz@gmail.com>
2021-10-06 11:58:36 -0700

    10.6 classwork

---
+++ Assets/scripts/CellularAutomataCubes.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellularAutomataCubes : MonoBehaviour
+{
+    [SerializeField] GameObject _primitive;
+    [SerializeField] Material _primitiveMaterial;
+
+    [SerializeField] ComputeShader _shader;
+    [SerializeField] ComputeShader _clearShader;
+    RenderTexture _result;
+
+    [SerializeField] int _width = 128;
+    [SerializeField] int _height = 128;
+
+    [SerializeField, Range(0, 255)] int _rule;
+
+    [SerializeField] bool _drawDebugTexture;
+
+    int threadDim = 8;
+    int _frameNum = 0;
+
+    void Start()
+    {
+        _result = new RenderTexture(_width, _height, 0);
+        _result.enableRandomWrite = true;
+        _result.Create();
+
+        var cubeParent = new GameObject("cubes");
+
+        for (int x = 0; x < _width; x++)
+        {
+            for (int y = 0; y < _height; y++)
+            {
+                var obj = GameObject.Instantiate(_primitive, cubeParent.transform);
+                obj.transform.position = new Vector3(
+                    x - _width / 2,
+                    0,
+                    y - _height / 2);
+
+                obj.transform.localScale = Vector3.one * 0.9f;
+
+                obj.GetComponent<MeshRenderer>().material = _primitiveMaterial;
+
+                var block = new MaterialPropertyBlock();
+                block.SetVector("_xy", new Vector2(
+                    (float)(x + 0.5) / 128.0f,
+                    (float)(y + 0.5) / 128.0f));
+                obj.GetComponent<MeshRenderer>().SetPropertyBlock(block);
+            }
+        }
+
+        _primitiveMaterial.SetTexture("_cellularData", _result);
+    }
+
+
+    private void OnDestroy()
+    {
+        if (_result != null) { _result.Release(); }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _frameNum = 0;
+            _clearShader.SetTexture(0, "_Result", _result);
+            _clearShader.Dispatch(0, _width / threadDim, _height / threadDim, 1);
+            _rule = (int)(Random.value * 255);
+        }
+
+        if (_frameNum < 512)
+        {
+            _shader.SetInt("_FrameNum", _frameNum);
+            _shader.SetInt("_Width", _width);
+            _shader.SetInt("_Height", _height);
+
+            _shader.SetInt("_Rule", _rule);
+
+            _shader.SetTexture(0, "_Result", _result);
+            _shader.Dispatch(0, _width / threadDim, 1, 1);
+            _frameNum++;
+        }
+
+    }
+
+    private void OnGUI()
+    {
+
+        if (_drawDebugTexture == true)
+        {
+            GUI.DrawTexture(new Rect(0, 0, _width * 3, _height * 3), _result);
+        }
+    }
+
+}
---
+++ Assets/shader/instanceCubeShader.shader
@@ -0,0 +1,62 @@
+Shader "Custom/instanceCubeShader"
+{
+    Properties
+    {
+        _Color ("Color", Color) = (1,1,1,1)
+        _MainTex ("Albedo (RGB)", 2D) = "white" {}
+        _Glossiness ("Smoothness", Range(0,1)) = 0.5
+        _Metallic ("Metallic", Range(0,1)) = 0.0
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 200
+
+        CGPROGRAM
+        // Physically based Standard lighting model, and enable shadows on all light types
+        #pragma surface surf Standard fullforwardshadows addshadow
+            
+
+        // Use shader model 3.0 target, to get nicer looking lighting
+        #pragma target 3.0
+
+        sampler2D _MainTex;
+        sampler2D _cellularData;
+
+        struct Input
+        {
+            float2 uv_MainTex;
+        };
+
+        half _Glossiness;
+        half _Metallic;
+        fixed4 _Color;
+
+       // instanced properties
+        UNITY_INSTANCING_BUFFER_START(Props)
+        UNITY_DEFINE_INSTANCED_PROP(float2, _xy)
+        UNITY_INSTANCING_BUFFER_END(Props)
+
+        void surf (Input IN, inout SurfaceOutputStandard o)
+        {
+            // Albedo comes from a texture tinted by color
+            fixed4 c = tex2D (_MainTex, IN.uv_MainTex) * _Color;
+            
+            //color by instanced props
+            float2 uv = UNITY_ACCESS_INSTANCED_PROP(Props, _xy);
+            float3 alive = tex2D(_cellularData, uv).rgb;
+
+            //clips black cubes, black cubes will have 0 opacity
+            clip(alive.x - 0.5);
+
+            o.Albedo = alive;
+
+            // Metallic and smoothness come from slider variables
+            o.Metallic = _Metallic;
+            o.Smoothness = _Glossiness;
+            o.Alpha = c.a;
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
